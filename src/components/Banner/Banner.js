d49ef91195ae2495b970cc0825612aa56a748a1d
unknown <ssgsj.carranza@gmail.com>
2021-06-11 11:30:33 -0500

    created truncate function to cut of movie description after 150 characters

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,6 +2,10 @@ import React from 'react'
 import './Banner.css'
 
 function Banner() {
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+    }
+    
     return (
         <header className='banner' style={{backgroundPosition: 'center center', backgroundSize:'cover', backgroundImage:`url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`}}>
             <div className='banner__contents'>
@@ -10,7 +14,7 @@ function Banner() {
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>My List</button>
                 </div>
-                <h1 className='banner__description'>Test description</h1>
+                <h1 className='banner__description'>{truncate(`Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description`, 150)}</h1>
             </div>
             <div className='banner--fadeBottom'/>
         </header>
