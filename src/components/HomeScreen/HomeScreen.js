85e03eaa17dcf48d8e3e01213ba99837c5191b16
unknown <ssgsj.carranza@gmail.com>
2021-06-11 10:30:08 -0500

    styled nav bar working on disappear on scroll functionality

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -6,6 +6,25 @@ function HomeScreen() {
     return (
         <div className='homeScreen'>
             <Nav />
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
             {/* Banner */}
             {/* Rows */}
         </div>
