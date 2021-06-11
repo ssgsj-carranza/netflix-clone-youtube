633e69049c0a13bfeafe7c7d41b552d151b8cb70
unknown <ssgsj.carranza@gmail.com>
2021-06-11 11:17:56 -0500

    fixed issue with white space and began styling banner

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,31 +1,13 @@
 import React from 'react'
 import './HomeScreen.css';
 import Nav from '../Nav/Nav';
+import Banner from  '../Banner/Banner'
 
 function HomeScreen() {
     return (
         <div className='homeScreen'>
             <Nav />
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            {/* Banner */}
+            <Banner />
             {/* Rows */}
         </div>
     )
