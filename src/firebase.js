4c99c2c2833b5840bef85453a15005fc25bb3c61
unknown <ssgsj.carranza@gmail.com>
2021-06-10 23:15:42 -0500

    initial project set up, set up firebase back end with email and password authentication

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5M2LEu_qjFd-qwX2o8euw5f1-_6TmE34",
+    authDomain: "netflix-clone-6e211.firebaseapp.com",
+    projectId: "netflix-clone-6e211",
+    storageBucket: "netflix-clone-6e211.appspot.com",
+    messagingSenderId: "688737872012",
+    appId: "1:688737872012:web:93bdbb5ad4a78d808233a9"
+  };
