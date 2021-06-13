5dc1fba3bfcfdac93b4e495ca5098d5e615ce4da
unknown <ssgsj.carranza@gmail.com>
2021-06-12 23:23:53 -0500

    created row component to render a collection of shows and movies titles, managed to only pull info into an array for now, working on display next

---
+++ src/components/Row/Row.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react'
+import './Row.css'
+import axios from '../../axios'
+import requests from '../../requests'
+
+function Row({title, fetchUrl, isLargeRow=false}) {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl])
+    console.log(movies)
+    
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+        </div>
+    )
+}
+
+export default Row
