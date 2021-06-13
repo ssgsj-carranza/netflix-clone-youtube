5c3d9d62ac5151afdfe5bb35f05591268086fa83
unknown <ssgsj.carranza@gmail.com>
2021-06-12 23:33:35 -0500

    pulled images from the list of I got back from the row component function

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -5,6 +5,7 @@ import requests from '../../requests'
 
 function Row({title, fetchUrl, isLargeRow=false}) {
     const [movies, setMovies] = useState([])
+    const base_url = 'https://image.tmdb.org/t/p/original/';
 
     useEffect(() => {
         async function fetchData(){
@@ -19,6 +20,12 @@ function Row({title, fetchUrl, isLargeRow=false}) {
     return (
         <div className='row'>
             <h2>{title}</h2>
+            {movies.map((movie) => (
+                <img 
+                    className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                    key={movie.id} 
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+            ))}
         </div>
     )
 }
