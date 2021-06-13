671ef40c608e6095174e978bde0b5fb5d40fba30
unknown <ssgsj.carranza@gmail.com>
2021-06-12 23:04:40 -0500

    pulled movie or show description as well to render on banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -26,12 +26,12 @@ function Banner() {
                                            backgroundSize:'cover', 
                                            backgroundImage:`url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`}}>
             <div className='banner__contents'>
-                <h1 className='banner__title'> Movie Name</h1>
+                <h1 className='banner__title'>{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className='banner__buttons'>
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>My List</button>
                 </div>
-                <h1 className='banner__description'>{truncate(`Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description Test Description`, 150)}</h1>
+                <h1 className='banner__description'>{truncate(movie?.overview, 150)}</h1>
             </div>
             <div className='banner--fadeBottom'/>
         </header>
