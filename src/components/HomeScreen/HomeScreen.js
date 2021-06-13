7feab6d6292a97252442367cd3b13a25275eb1a5
unknown <ssgsj.carranza@gmail.com>
2021-06-12 23:55:21 -0500

    pulled show and movie image for each category and styled it, top rated link seemed to be broken so for now it has been commented out

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -15,7 +15,7 @@ function HomeScreen() {
                 fetchUrl={requests.fetchNetflixOirignals}
                 isLargeRow/>
             <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
-            <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+            {/* <Row title='Top Rated' fetchUrl={requests.fetchTopRated} /> */}
             <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
             <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
             <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
