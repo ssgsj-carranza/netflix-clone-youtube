c82b450b76944057cdb5a602e6c369daa878eee5
unknown <ssgsj.carranza@gmail.com>
2021-06-14 14:59:07 -0500

    started styling of login in page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen/HomeScreen'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import LoginScreen from './components/LoginScreen/LoginScreen';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
       <Router>
-        <Switch>
-          <Route path='/'>
-            <HomeScreen />
-          </Route>
-        </Switch>
-     </Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path='/'>
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+     </Router>)
     </div>
   );
 }
