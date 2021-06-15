0b5f049956dc92fc9a2f0079be118ca86c71f39c
unknown <ssgsj.carranza@gmail.com>
2021-06-15 13:06:58 -0500

    styled plans page, project pretty much done, ran into a bug with page not showing which package user is subscribed to

--- src/components/PlansScreen/PlansScreen.js
+++ src/components/PlansScreen/PlansScreen.js
@@ -1,9 +1,28 @@
 import React, {useState, useEffect} from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
 import db from '../../firebase';
 import './PlansScreen.css'
+import {loadStripe} from '@stripe/stripe-js';
 
 function PlansScreen() {
     const [products, setProducts] = useState([]);
+    const user = useSelector(selectUser);
+    const [subscription, setSubscription] = useState(null);
+
+    useEffect(() => {
+        db.collection('customers').doc(user.uid) .collection('subscriptions').get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(async subscription => {
+                setSubscription({
+                    role: subscription.data().role,
+                    current_period_end: subscription.data().current_period_end.seconds,
+                    current_period_start: subscription.data().current_period_start.seconds,
+                });
+            });
+        });       
+    },[user.uid]);
+    console.log(subscription)
 
     useEffect(() =>{
         db.collection('products').where('active', '==', true)
@@ -24,17 +43,41 @@ function PlansScreen() {
     },[])
     console.log(products);
 
+    const loadCheckout = async(priceId) => {
+        const docRef = await db.collection('customers').doc(user.uid).collection('checkout_sessions')
+        .add({
+            price: priceId,
+            success_url: window.location.origin,
+            cancel_url: window.location.origin,
+        });
+        docRef.onSnapshot(async(snap) => {
+            const {error, sessionId} = snap.data();
+            if (error) {
+                alert(`An error occured: ${error.message}`);
+            }
+            if (sessionId) {
+                const stripe = await loadStripe('pk_test_51J2df7CTVwmN6IjA7mwtyhNONamWZ63RtYAxFB47stlHKJGifB5iQ2YjVZf3XirUuTQZwcjsNIx84eI0cwjmGjT900AkAyUrCQ');
+                stripe.redirectToCheckout({sessionId});
+            }
+        })                                
+    };
+
     return (
         <div className='plansScreen'>
+            <br/>
+            {subscription && <p>Renewal date: {new Date(subscription?.current_period_end * 1000).toLocaleDateString()}</p>}
             {Object.entries(products).map(([productId, productData]) => {
-                //TODO: add logic top check user subscription is active
+                const isCurrentPackage = productData.name?.toLowerCase().includes(subscription?.role);
+                console.log(isCurrentPackage, 'is current ')
                 return (
-                   <div className='plansScreen__plan'>
+                   <div key={productId} className={`${isCurrentPackage && 'plansScreen__plan--disabled'} plansScreen__plan`}>
                        <div className='plansScreen__info'>
                            <h5>{productData.name}</h5>
                            <h6>{productData.description}</h6>
                        </div>
-                       <button>Subscribe</button>
+                       <button onClick={() => !isCurrentPackage && loadCheckout(productData.prices.priceId)}>
+                           {isCurrentPackage ? 'Current Package' : 'Subscribe'}
+                        </button>
                    </div> 
                 );
             })}
