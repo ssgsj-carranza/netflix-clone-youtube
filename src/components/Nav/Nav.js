9923fea18a326a3f3e8fffdfc3e46c60a6f098f5
unknown <ssgsj.carranza@gmail.com>
2021-06-14 23:03:03 -0500

    linked user avatar to profile screen page using useHisotry

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react'
+import { useHistory } from 'react-router-dom';
 import './Nav.css'
 
 function Nav() {
     const [show, handleShow] = useState(false);
+    const history = useHistory();
     
     const transitionNavBar = () => {
         if (window.scrollY > 100) {
@@ -21,7 +23,7 @@ function Nav() {
         <div className={`nav ${show && 'nav__black'}`}>
             <div className='nav__contents'>
                 <img className='nav__logo' src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt=''/>
-                <img className='nav__avatar' src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png' alt=''/>
+                <img onClick={() => history.push('/profile')} className='nav__avatar' src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png' alt=''/>
             </div>
         </div>
     )
