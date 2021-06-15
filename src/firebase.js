01d35ca05f885717572dd6afe34ce8b6603733e4
unknown <ssgsj.carranza@gmail.com>
2021-06-14 22:22:21 -0500

    set up firebase and firestore to authenticate user, set up prevent defaults for my sign in and register methods. register function is succesful was able to pull user data

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyA5M2LEu_qjFd-qwX2o8euw5f1-_6TmE34",
     authDomain: "netflix-clone-6e211.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "688737872012",
     appId: "1:688737872012:web:93bdbb5ad4a78d808233a9"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {auth};
+  export default db;
