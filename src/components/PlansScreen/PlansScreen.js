c3cd4ea4a9faea4a9dc85b01b35a57d69b04b8e0
unknown <ssgsj.carranza@gmail.com>
2021-06-15 13:22:34 -0500

    refactored subscribe button

--- src/components/PlansScreen/PlansScreen.js
+++ src/components/PlansScreen/PlansScreen.js
@@ -75,9 +75,10 @@ function PlansScreen() {
                            <h5>{productData.name}</h5>
                            <h6>{productData.description}</h6>
                        </div>
-                       <button onClick={() => !isCurrentPackage && loadCheckout(productData.prices.priceId)}>
+                       {/* <button onClick={() => !isCurrentPackage && loadCheckout(productData.prices.priceId)}>
                            {isCurrentPackage ? 'Current Package' : 'Subscribe'}
-                        </button>
+                        </button> */}
+                        <button onClick={() => loadCheckout(productData.prices.priceId)}>Subscribe</button>
                    </div> 
                 );
             })}
