aaad8ddd34f036227a1fa49a4ec31bc1dcae548f
unknown <ssgsj.carranza@gmail.com>
2021-06-14 21:30:15 -0500

    login page styled

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -8,6 +8,19 @@ function LoginScreen() {
                 <img className='loginScreen__logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt=''/>
                 <button className='loginScreen__button'>Sign In</button>
                 <div className='loginScreen__gradient'></div>
+                <div className='loginScreen__body'>
+                    <>
+                        <h1>Unlimited films, TV programmes and more.</h1>
+                        <h2>Watch anywhere, Cancel at any time.</h2>
+                        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                        <div className='loginScreen__input'>
+                            <form>
+                                <input type='email' placeholder='Email Adress'/>
+                                <button className='loginScreen__getStarted'>GET STARTED</button>
+                            </form>
+                        </div>
+                    </>
+                </div>
             </div>
         </div>
     )
