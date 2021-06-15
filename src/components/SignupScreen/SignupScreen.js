05562ca583eb639a4ce9e2d7d3d02ea6a2670471
unknown <ssgsj.carranza@gmail.com>
2021-06-14 22:46:04 -0500

    refactored counterSlice to user slice and refactored code within, made correct chaanges to store js

--- src/components/SignupScreen/SignupScreen.js
+++ src/components/SignupScreen/SignupScreen.js
@@ -8,10 +8,12 @@ function SignupScreen() {
 
     const register = (e) => {
         e.preventDefault();
+        
         auth.createUserWithEmailAndPassword(
             emailRef.current.value,
             passwordRef.current.value
-        ).then((authUser) => {
+        )
+        .then((authUser) => {
             console.log(authUser);
         })
          .catch((error) => {
@@ -21,13 +23,24 @@ function SignupScreen() {
 
     const signIn = (e) => {
         e.preventDefault();
+
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then((authUser) => {
+            console.log(authUser)
+        })
+        .catch((error) => {
+            alert(error.message)
+        })
     }
 
     return (
         <div className='signupScreen'>
             <form>
                 <h1>Sign In</h1>
-                <input ref={emailRef}placeholder='Email' type='email' />
+                <input ref={emailRef} placeholder='Email' type='email' />
                 <input ref={passwordRef} placeholder='Password' type='password'/>
                 <button type='submit' onClick={signIn}>Sign In</button>
                 <h4>
