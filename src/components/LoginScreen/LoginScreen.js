b5eb8b1d3d2b225263f9f0aa5c5a7d7caeac0324
unknown <ssgsj.carranza@gmail.com>
2021-06-14 21:45:33 -0500

    set conditional on login screen to redirect user to log in sign in page, started work on sign in page

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './LoginScreen.css'
+import SignupScreen from '../SignupScreen/SignupScreen';
 
 function LoginScreen() {
+    const [signIn, setSignIn] = useState(false);
+
     return (
         <div className='loginScreen'>
             <div className='loginScreen__background'>
                 <img className='loginScreen__logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt=''/>
-                <button className='loginScreen__button'>Sign In</button>
+                <button className='loginScreen__button' onClick={() => setSignIn(true)}>Sign In</button>
                 <div className='loginScreen__gradient'></div>
                 <div className='loginScreen__body'>
+                    {signIn ? (
+                        <SignupScreen />
+                    ) : (
                     <>
                         <h1>Unlimited films, TV programmes and more.</h1>
                         <h2>Watch anywhere, Cancel at any time.</h2>
@@ -16,10 +22,11 @@ function LoginScreen() {
                         <div className='loginScreen__input'>
                             <form>
                                 <input type='email' placeholder='Email Adress'/>
-                                <button className='loginScreen__getStarted'>GET STARTED</button>
+                                <button className='loginScreen__getStarted' onClick={() => setSignIn(true)}>GET STARTED</button>
                             </form>
                         </div>
                     </>
+                    )}
                 </div>
             </div>
         </div>
---
+++ src/components/SignupScreen/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SignupScreen.css'
+
+function SignupScreen() {
+    return (
+        <div className='singupScreen'>
+            
+        </div>
+    )
+}
+
+export default SignupScreen
